bb35b4ee49f050466afc13d121dc4336b49f327b
ParthivC05 <parthiv@pervadorit.com>
2025-07-01 17:11:26 +0530

    Deployment Issue2

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -16,7 +16,7 @@ const LoginPage = () => {
     e.preventDefault();
     setMessage("");
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/login`, {
+      const res = await fetch(`${import.meta.env.VITE_AUTH_API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -27,7 +27,7 @@ const RegisterPage = () => {
       return;
     }
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/register`, {
+      const res = await fetch(`${import.meta.env.VITE_AUTH_API_URL}/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
