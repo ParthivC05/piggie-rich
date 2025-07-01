5d41d848fae00a877d27c5800eab70e45c2178d8
ParthivC05 <parthiv@pervadorit.com>
2025-07-01 11:15:39 +0530

    newtab issue

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,29 +1,11 @@
 import React from "react";
-import GameEmbed from "./GameEmbed";
 
 const GameCard = ({ game }) => {
   const token = import.meta.env.VITE_API_TOKEN;
 
   const handlePlayNow = () => {
     const iframeUrl = `https://slotslaunch.com/iframe/${game.id}?token=${token}`;
-    const newWindow = window.open("", "_blank", "noopener,noreferrer,width=900,height=700");
-    if (newWindow) {
-      newWindow.document.write(`
-        <html>
-          <head>
-            <title>${game.name}</title>
-            <style>
-              body { margin: 0; background: #000; }
-              iframe { width: 100vw; height: 100vh; border: none; }
-            </style>
-          </head>
-          <body>
-            <iframe src="${iframeUrl}" allowfullscreen frameborder="0" title="Game Frame"></iframe>
-          </body>
-        </html>
-      `);
-      newWindow.document.close();
-    }
+    window.open(iframeUrl, "_blank", "noopener,noreferrer,width=900,height=700");
   };
 
   return (
